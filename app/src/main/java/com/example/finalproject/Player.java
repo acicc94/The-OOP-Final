c2fdc551483fc585c48aa9381cd3c02061609bd2
DaltonCobb <61435085+DaltonCobb@users.noreply.github.com>
2020-03-02 14:26:42 -0700

    player stats and stats

--- app/src/main/java/com/example/finalproject/Player.java
+++ app/src/main/java/com/example/finalproject/Player.java
@@ -1,4 +1,20 @@
 package com.example.finalproject;
 
-public class Player {
+public class Player extends Stats{
+    private String name;
+
+    public Player(int health, int speed, int availablePoint) {
+        super(health, speed, availablePoint);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name.trim().isEmpty()){
+            throw new IllegalArgumentException("Name cant be null, empty, or whitespace-only.");
+        }
+        this.name = name;
+    }
 }
--- app/src/main/java/com/example/finalproject/Stats.java
+++ app/src/main/java/com/example/finalproject/Stats.java
@@ -1,4 +1,43 @@
 package com.example.finalproject;
 
 public class Stats {
+    protected int health;
+    protected int speed;
+    private int AvailablePoint;
+
+    public Stats(int health, int speed, int availablePoint) {
+        setHealth(health);
+        setSpeed(speed);
+        setAvailablePoint(availablePoint);
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        if(health <= 0){
+            throw new IllegalArgumentException("Health cant be 0 or less. ");
+        }
+        this.health = health;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        if(speed < 0){
+            throw new IllegalArgumentException("speed cant be less than 0. ");
+        }
+        this.speed = speed;
+    }
+
+    public int getAvailablePoint() {
+        return AvailablePoint;
+    }
+
+    public void setAvailablePoint(int availablePoint) {
+        AvailablePoint = availablePoint;
+    }
 }
