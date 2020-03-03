534aeb4b2950ba0bff3c8950a6254a025ce87a51
leland <61434998+lvcl1@users.noreply.github.com>
2020-03-03 14:39:44 -0700

    Combat and Outside

--- app/src/main/java/com/example/finalproject/Combat.java
+++ app/src/main/java/com/example/finalproject/Combat.java
@@ -1,19 +1,27 @@
 package com.example.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
 
 
 public class Combat extends AppCompatActivity{
+    private TextView playerHealth,EnemyHp,log;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.combat);
+        playerHealth=findViewById(R.id.playerhp);
+        EnemyHp=findViewById(R.id.Enemyhp);
+        log=findViewById(R.id.log);
     }
     public void run(){
-
+        Intent intent = new Intent(this, Outside.class);
+        startActivity(intent);
     }
     public void attack(){
 
--- app/src/main/java/com/example/finalproject/Outside.java
+++ app/src/main/java/com/example/finalproject/Outside.java
@@ -3,23 +3,35 @@ package com.example.finalproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.Random;
 
 public class Outside extends AppCompatActivity{
+    private TextView showevent;
     Random random=new Random();
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.outside);
+        showevent=findViewById(R.id.showevent);
     }
     public void search(View v) {
-        int nomOfEnemies = random.nextInt(5) + 1;
-
-
+        int nomOfEnemies = random.nextInt(5) + 1,eventNum=random.nextInt(5);
+        String event;
+        if (eventNum==0){
+            //TODO replace " enemies" bellow with name of enemy
+            event="you found "+nomOfEnemies+" enemies";
+            showevent.setText(event);
+        }else {
+            event="nothing happened";
+            showevent.setText(event);
+        }
     }
     public void headback(View v){
-
+        Intent intent = new Intent(this, InTown.class);
+        startActivity(intent);
     }
     public void fight(View v){
         Intent intent = new Intent(this, Combat.class);
