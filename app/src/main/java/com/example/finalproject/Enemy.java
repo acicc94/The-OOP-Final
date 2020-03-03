6683f9d68e20aefd8b2f77289a63a64c79b6ddfb
acicc94 <60477377+acicc94@users.noreply.github.com>
2020-03-03 13:42:46 -0700

    Added code to shop

--- app/src/main/java/com/example/finalproject/Enemy.java
+++ app/src/main/java/com/example/finalproject/Enemy.java
@@ -4,4 +4,8 @@ public class Enemy extends Stats {
 
     private int newEnemy;
 
+    public Enemy(int health, int speed, int availablePoint, int newEnemy) {
+        super(health, speed, availablePoint);
+        this.newEnemy = newEnemy;
+    }
 }
