19372300ea9eabf5a7822836b77e11bc4c0513ec
leland <61434998+lvcl1@users.noreply.github.com>
2020-03-04 14:25:58 -0700

    Combat and Outside

--- app/src/main/java/com/example/finalproject/Enemy.java
+++ app/src/main/java/com/example/finalproject/Enemy.java
@@ -1,18 +1,13 @@
 package com.example.finalproject;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 
 public class Enemy extends Stats {
-
-    Random stats = new Random();
+    Random random=new Random();
 
     public Enemy(int health, int speed, int attack, int availablePoint) {
         super(health, speed, attack, availablePoint);
     }
-    Random random=new Random();
     public String Newenemy() {
         int newenemy = random.nextInt(4);
         if (newenemy == 0) {
