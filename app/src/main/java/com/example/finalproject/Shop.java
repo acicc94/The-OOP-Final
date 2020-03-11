2369653db2022d26655d64382ba95d3709499fa9
leland <61434998+lvcl1@users.noreply.github.com>
2020-03-11 11:30:13 -0600

    Combat and Outside

--- app/src/main/java/com/example/finalproject/Shop.java
+++ app/src/main/java/com/example/finalproject/Shop.java
@@ -11,7 +11,7 @@ import static com.example.finalproject.MainActivity.*;
 import java.util.Random;
 
 public class Shop extends AppCompatActivity{
-    private TextView shopEvent,goldamount;
+    private TextView shopEvent, goldAmount,weaponCost,armorCost;
     private Button armor,weapon;
     private Random cost = new Random();
     private int weaponAmount,armorAmount;
@@ -21,14 +21,18 @@ public class Shop extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shop);
         shopEvent =findViewById(R.id.showevent);
-        goldamount=findViewById(R.id.goldamount);
+        goldAmount =findViewById(R.id.goldamount);
+        weaponCost=findViewById(R.id.textView4);
+        armorCost=findViewById(R.id.textView3);
         armor=findViewById(R.id.button2);
         weapon=findViewById(R.id.button8);
-        goldamount.setText("you have "+gold);
+        goldAmount.setText("you have "+gold);
         weaponAmount=(cost.nextInt(attack)*50+cost.nextInt(level)*10);
         armorAmount=(cost.nextInt(defense+1)*50+cost.nextInt(level)*10);
         goldCheck(weaponAmount);
         goldCheck(armorAmount);
+        weaponCost.setText("weapon cost "+weaponAmount);
+        armorCost.setText("armor cost "+armorAmount);
     }
 
     public void shopTalk(View v){
@@ -40,14 +44,14 @@ public class Shop extends AppCompatActivity{
     public void upgradeWeapon(View v){
         attack++;
         gold-=weaponAmount;
-        goldamount.setText("you have "+gold);
+        goldAmount.setText("you have "+gold);
         goldCheck(weaponAmount);
     }
 
     public void upgradeArmor(View v){
         defense++;
         gold-=armorAmount;
-        goldamount.setText("you have "+gold);
+        goldAmount.setText("you have "+gold);
         goldCheck(armorAmount);
     }
 
@@ -64,13 +68,5 @@ public class Shop extends AppCompatActivity{
             armor.setEnabled(true);
             weapon.setEnabled(true);
         }
-    }
-
-    public TextView getShopEvent() {
-        return shopEvent;
-    }
-
-    public void setShopEvent(TextView shopEvent) {
-        this.shopEvent = shopEvent;
     }
 }
